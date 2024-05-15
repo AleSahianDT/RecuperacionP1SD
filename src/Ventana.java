814c0be27f732a07a27e119f669527bb94bb62a0
bernarda <bernarda.sandoval@udla.edu.ec>
2024-05-15 16:31:23 -0500

    interfa<

---
+++ src/Ventana.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Ventana {
+    private JPanel Ventana;
+    private JButton encolarButton;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JTextField textField2;
+    private JComboBox comboBox2;
+    private JTextArea textArea1;
+    private JButton restarButton;
+    private JButton separarButton;
+    private JTextArea textArea2;
+    private JTextArea textArea3;
+}
